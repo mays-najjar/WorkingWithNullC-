4a6c60b192e583c88926726486f2afa60a6c1653
mays-najjar <mays.najjar2001@gmail.com>
2025-08-04 09:18:24 +0300

    Start working of null in C# and handle it

---
+++ ConsoleApp1/PersonModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsMarried { get; set; }
+        
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+int? i = null;
+string s = null;
+bool? found ;
+PersonModel person = null ;
+if (s is not null)
+{
+    Console.WriteLine(s.Length);
+}
+Console.WriteLine(person?.Name);
