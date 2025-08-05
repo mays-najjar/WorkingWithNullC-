9e6202b2b6779beb049fd4303bae86ce3f27de2c
mays-najjar <mays.najjar2001@gmail.com>
2025-08-05 17:43:56 +0300

    More examples of nullable types in C#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp1;
+// This is a simple to teach you the types of nullable and where to use them.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Nullabe lesson!");
+
+// Nullable types can be used with value types and reference types.
+// 1. Nullable Value Type ?
+// Value types are not nullable by default, so we can use Nullable<T> or the shorthand T? to make them nullable.
 int? i = null;
-string s = null;
 bool? found ;
-PersonModel person = null ;
-if (s is not null)
+if (i.HasValue)
 {
-    Console.WriteLine(s.Length);
+    Console.WriteLine(i.Value);
 }
-Console.WriteLine(person?.Name);
+
+// 2. Nullable Reference Type
+// Reference types are nullable by default in C# 8.0, so we want to enable nullable reference types 
+// in the project file to get warnings for nullability issues. 
+#nullable enable
+PersonModel? person = null;
+if (person != null)
+{
+    Console.WriteLine(person.Name);
+}
+else
+{
+    Console.WriteLine("Person is null");
+}
+string? name = null;
+Console.WriteLine(name?.Length); // If name is: not null: it returns name.Length ,, is null: it returns null (and avoids a crash!)
+
+// 3. Using null-coalescing operator ??
+string? nullableString = null;
+string nonNullableString = nullableString ?? "Default Value"; // If nullableString is null, it assigns "Default Value"
+Console.WriteLine(nonNullableString);
+
+// 4. Using Null-Coalescing Assignment (??=)
+string? anotherNullableString = null;
+
+anotherNullableString ??= "Assigned Value"; // If anotherNullableString is null, it assigns "Assigned Value"
+
+// Null-Forgiving Operator (!) 
+Console.WriteLine(anotherNullableString!); // Using ! to assert that anotherNullableString is not null, but be cautious with this!
+
+// Null-Conditional Operator (?.)
+PersonModel? person1 = null;
+int? length = person1?.Name?.Length;
+
+#nullable disable
+string name2 = null; // No warning
+
+#nullable enable
+string? anotherName = null; // Compiler gives a warning if you use it unsafely
+
+#nullable restore
+string? thirdName = null; // Behavior goes back to project’s setting (enable or disable)
+
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6c60b192e583c88926726486f2afa60a6c1653")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
